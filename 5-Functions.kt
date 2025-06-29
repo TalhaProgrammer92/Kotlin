00e696d650d2e3e6bfa0d1f72cfb9f95f27c436f
TalhaProgrammer92 <talha.code92@gmail.com>
2025-06-29 23:18:48 +0500

    .

--- 5-Functions.kt
+++ 5-Functions.kt
@@ -1,24 +1,21 @@
 // * Get sum
-fun get_sum(x: Int, y: Int) Int
-{
+fun get_sum(x: Int, y: Int): Int {
     return x + y
 }
 
 // * Get integer input from user
-fun get_input(message: String) Int
-{
-    print(message)
-    val x = readln.toInt()
+fun get_input(message: String?): Int {
+    print("$message")
+    val x: Int = readln().toInt()
     return x
 }
 
-fun main()
-{
+fun main() {
     // * Get two numbers from user
-    val a = get_input("Enter first number: ")
-    val b = get_input("Enter second number: ")
+    val a: Int = get_input("Enter first number: ")
+    val b: Int = get_input("Enter second number: ")
 
     // * Show sum
-    val s = sum(a, b)
-    println("a + b = $a + $b = ")
+    val s: Int = get_sum(a, b)
+    println("a + b = $a + $b = $s")
 }
